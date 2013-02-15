02d74d5cc8cd634a91524ca89e3b5e315596b122
brandwarbs <bwarbeltron@gmail.com>
2013-02-15 13:58:29 -0600

    first

---
+++ Game1/src/TestShapes.java
@@ -0,0 +1,22 @@
+
+import TurtleGraphics.*;
+import java.awt.Color;
+import java.util.Scanner;
+
+public class TestShapes 
+{
+
+   public static void main (String[] args)
+   {
+
+      
+      
+      // Pause until the user is ready to continue
+      System.out.print("Press <Enter> to continue: ");
+      Scanner reader = new Scanner(System.in);
+      reader.nextLine();
+      
+   }
+
+}
+
